2f0083ccd7248bb7bed659e9d5dbbbeb2fd26d29
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-07 12:46:27 -0600

    console

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // asks the user to input a character and assigns input to variable
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey() .KeyChar;
+
+            // checks if the character user entered is a letter
+            bool isLetter = Char.IsLetter(myChar);
+
+            // prints results to console
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}");
+
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Enter a word.");
+            string word = Console.ReadLine();
+
+            bool isIn = sentence.Contains(word);
+
         }
     }
 }
